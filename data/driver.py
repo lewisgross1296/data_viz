dc856cf4f90a93e9d71a4163cb983d9d050da39d
Lewis Gross <ligross@wisc.edu>
2020-10-07 22:35:23 -0500

    infrastructure for extracting data

---
+++ data/driver.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys, os
+from optparse import *
+from extractor import extract
+
+if __name__ == "__main__":
+
+    # Parse the command line arguments
+    parser = OptionParser()
+    parser.add_option("--rendezvous_file", type="string", dest="rendezvous_file",
+                      help="the rendezvous file to load")
+    parser.add_option("--estimator_id", type="int", dest="estimator_id",
+                      help="the estimator id to use")
+    parser.add_option("--entity_id", type="int", dest="entity_id",
+                      help="the entity id to use")
+    parser.add_option("--mcnp_file", type="string", dest="mcnp_file",
+                      help="the mcnp output file to load")
+    parser.add_option("--mcnp_file_start", type="int", dest="mcnp_file_start",
+                      help="the mcnp output file start line")
+    parser.add_option("--mcnp_file_start", type="int", dest="mcnp_file_start",
+                      help="the mcnp output file start line")
+    parser.add_option("--current", action="store_true", dest="is_a_current",
+                      help="the data corresponds to a current")
+    parser.add_option("--flux", action="store_false", dest="is_a_current",
+                      help="the data corresponds to a flux")
+    options,args = parser.parse_args()
+
+    # extract the relevant data
+    extract(  options.rendezvous_file,
+              options.estimator_id,
+              options.entity_id,
+              options.mcnp_file,
+              options.mcnp_file_start,
+              options.mcnp_file_end,
+              options.is_a_current):
--- data/extractor.py
+++ data/extractor.py
@@ -11,19 +11,13 @@ import PyFrensie.MonteCarlo.Event as Event
 import PyFrensie.MonteCarlo.Manager as Manager
 from spectrum_plot_tools import plotSpectralDataWithErrors
 
-def plotSphereSimulationSpectrum( rendezvous_file,
-                                  estimator_id,
-                                  entity_id,
-                                  mcnp_file,
-                                  mcnp_file_start,
-                                  mcnp_file_end,
-                                  is_a_current,
-                                  top_ylims = None,
-                                  bottom_ylims = None,
-                                  legend_pos = None ):
-
-    # Set the database path
-    Collision.FilledGeometryModel.setDefaultDatabasePath( os.environ['DATABASE_PATH'] )
+def extract(  rendezvous_file,
+              estimator_id,
+              entity_id,
+              mcnp_file,
+              mcnp_file_start,
+              mcnp_file_end,
+              is_a_current):
     
     # Reload the simulation
     manager = Manager.ParticleSimulationManagerFactory( rendezvous_file ).getManager()
@@ -46,30 +40,18 @@ def plotSphereSimulationSpectrum( rendezvous_file,
         mcnp_bin_data["e_up"].append( float(split_line[0]) )
         mcnp_bin_data["mean"].append( float(split_line[1]) )
         mcnp_bin_data["re"].append( float(split_line[2]) )
-        
-    output_file_name = "h1_sphere_"
-    output_file_names = []
 
-    if is_a_current:
-        output_file_names.append( output_file_name + "current.eps" )
-        output_file_names.append( output_file_name + "current.png" )
-        data_type = "Current"
-    else:
-        output_file_names.append( output_file_name + "flux.eps" )
-        output_file_names.append( output_file_name + "flux.png" )
-        data_type = "Flux"
-
-
-
-    # Plot the data
-#    plotSpectralDataWithErrors( "FRENSIE",
-#                                entity_bin_data,
-#                                "MCNP6",
-#                                mcnp_bin_data,
-#                                data_type,
-#                                True,
-#                                per_lethargy = is_a_current,
-#                                top_ylims = top_ylims,
-#                                bottom_ylims = bottom_ylims,
-#                                legend_pos = legend_pos,
-#                                output_plot_names = output_file_names )
+    if(is_a_current):
+        with open('current_comparison.txt', 'w') as f: 
+            print(entity_bin_data) 
+            print('\n')
+            print(mcnp_bin_data, file=f)
+    else
+        with open('flux_comparison.txt', 'w') as f:  
+            print(entity_bin_data) 
+            print('\n')
+            print(mcnp_bin_data, file=f)
+            
+    # ensure file is closed
+    print('file is closed? ')
+    print(f.close)
